840ab7fc59a7b1e2b5ad17fc886acf1149cd5bb3
Josiah Welch <jwelch11@huskers.unl.edu>
2026-01-15 11:09:13 -0600

    initialized lex()

--- src/lexer.h
+++ src/lexer.h
@@ -1,7 +1,11 @@
+#include <stdbool.h>
+
 void setID(struct Token* token, const char* id);
 void setValue(struct Token* token, const char* value);
 char* getID(struct Token* token);
 char* getValue(struct Token* token);
+const size_t BLOCK_SIZE;
+
 void test() {
 	struct Token token;
 	struct Token otherToken;
@@ -12,3 +16,14 @@ void test() {
 	printf("%s: %s\n", getID(&token), getValue(&token));
 	printf("%s: %s\n", getID(&otherToken), getValue(&otherToken));
 }
+
+
+
+Token* lex(const char* prog) {
+	uint16_t bufSize = 16;
+	char* buf = malloc(bufSize);
+	for (int i=0;i<strlen(prog);i++) {
+		buf[strlen(buf)] = prog[i];
+		if (strlen(buf) >= bufSize - 1) {
+			buf = realloc(bufSize + strlen(buf)* 2);
+		}
