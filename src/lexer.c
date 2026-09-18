7a064a1122f774cddd5cfc68735c3cfd6f9b98e4
Josiah Welch <josiah.isaiah.welch@gmail.com>
2026-09-18 12:54:29 -0500

    lexer: fixed math() and refactored the token adding logic into add_token()

--- src/lexer.c
+++ src/lexer.c
@@ -7,12 +7,30 @@
 
 #include <lexer.h>
 
+// Lexer token variables
+ssize_t tok_len;
+ssize_t tok_n = 0;
+token_t *tokens;
+
 // Start, end, and scan pointers
 static char *tok_start;
 static char *tok_end;
 static char *tok_scan;
 
 // Helper functions
+static void add_token(const ttype_t type, const char *end) {
+    const ssize_t value_len = end - tok_start;
+
+    tokens[tok_n].value = malloc(value_len + 1);
+    memcpy(tokens[tok_n].value, tok_start, value_len);
+    tokens[tok_n].value[value_len] = '\0';
+    tokens[tok_n].value_len = value_len;
+    tokens[tok_n].type = type;
+
+    tok_start = (char *)end;
+    tok_n++;
+}
+
 static int is_terminator(const char *ptr) {
     return *ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == '\r' || *ptr == '\0';
 }
@@ -23,10 +41,15 @@ static int string() {
         return TOK_INVALID;
     if (tok_scan == tok_start)
         tok_scan++;
-    for (;*tok_scan != '"' && tok_scan != tok_end; tok_scan++) // To ensure that there isn't a \ before the end quotation mark
-        if (*tok_scan == '\\')
+    while (tok_scan != tok_end && *tok_scan != '"') { // To ensure that there isn't a \ before the end quotation mark
+        if (*tok_scan == '\\' && tok_scan + 1 != tok_end)
             tok_scan++;
-    return *tok_scan++ == '"'; // Increments so that it includes the closing quotation mark
+        tok_scan++;
+    }
+    if (tok_scan == tok_end)
+        return TOK_INVALID;
+    tok_scan++;
+    return TOK_STRING;
 }
 
 static ttype_t identifier() {
@@ -34,7 +57,7 @@ static ttype_t identifier() {
     if ((*tok_start < 'a' || *tok_start > 'z') && (*tok_start < 'A' || *tok_start > 'Z')) // First character must be alphabetical
         return TOK_INVALID;
 
-    for (;!is_terminator(tok_scan); tok_scan++)
+    for (;tok_scan != tok_end && !is_terminator(tok_scan); tok_scan++)
         if ((*tok_scan < 'a' || *tok_scan > 'z') && (*tok_scan < 'A' || *tok_scan > 'Z') && (*tok_scan < '0' || *tok_scan > '9')) // Rest of them must be alphanumeric
             return TOK_INVALID;
 
@@ -112,7 +135,9 @@ static ttype_t math() {
     if (tok_scan - tok_start > 0)
         return TOK_INVALID;
 
-    switch (*(tok_scan + 1)) {
+    const char next = tok_scan + 1 < tok_end ? *(tok_scan + 1) : '\0';
+
+    switch (next) {
         case '+':
             return *tok_scan == '+' ? TOK_INC : TOK_INVALID;
             break;
@@ -134,7 +159,7 @@ static ttype_t math() {
                     return TOK_MINUS;
                     break;
                 case '*':
-                    return TOK_AMP;
+                    return TOK_STAR;
                     break;
                 case '/':
                     return TOK_SLASH;
@@ -169,52 +194,48 @@ static ttype_t math() {
  * @return a pointer to the lexed token array
  */
 token_t *lex(char *src, ssize_t len) {
-    // Handling token array
-    const ssize_t block = (len - len % 4) / 4; // ~1/4 of the source length
-    ssize_t tok_len = block;
-    ssize_t tok_n = 0;
-    token_t *tokens = malloc(sizeof(token_t) * tok_len);
-
+    tok_len = len + 1;
+    tokens = calloc(tok_len, sizeof(token_t));
+    tok_n = 0;
     tok_start = src;
     tok_end = src + len;
 
+    // Handling token array
+
     for (tok_scan = src; tok_scan - src < len; tok_scan++) {
         // Whitespace, newline, and tab handling
-        while ((is_terminator(tok_start)) && (src - tok_start) < len)
+        while (tok_start < tok_end && is_terminator(tok_start))
             tok_start++;
-        if ((is_terminator(tok_scan)) && (src - tok_scan) < len) // So that tok_scan is checked too
+        if (tok_scan < tok_end && is_terminator(tok_scan)) // So that tok_scan is checked too
             tok_start = tok_scan + 1;
 
         if (tok_scan < tok_start)
             tok_scan = tok_start;
+        if (tok_start == tok_end)
+            break;
 
         // String handling
         if (string()) {
-            tokens[tok_n].value = malloc(tok_scan - tok_start + 1);
-            strncpy(tokens[tok_n].value, tok_start, tok_scan - tok_start);
-            tokens[tok_n].type = TOK_STRING;
-            tok_start = tok_scan;
-            tok_n++;
+            add_token(TOK_STRING, tok_scan);
+            tok_scan--;
+            continue;
         }
 
         // Identifier handling
         const ttype_t id = identifier();
-        if (!!id) {
-            tokens[tok_n].value = malloc(tok_scan - tok_start + 1);
-            strncpy(tokens[tok_n].value, tok_start, tok_scan - tok_start);
-            tokens[tok_n].type = id;
-            tok_start = tok_scan;
-            tok_n++;
+        if (id != TOK_INVALID) {
+            add_token(id, tok_scan);
+            tok_scan--;
+            continue;
         }
 
         // Math symbol handling
         const ttype_t sym = math();
-        if (!!sym) {
-            tokens[tok_n].value = malloc(tok_scan - tok_start + 1);
-            strncpy(tokens[tok_n].value, tok_start, tok_scan - tok_start + 2);
-            tokens[tok_n].type = sym;
-            tok_start = tok_scan;
-            tok_n++;
+        if (sym != TOK_INVALID) {
+            const bool is_double = sym == TOK_INC || sym == TOK_DEC || sym == TOK_SHIFT_LEFT || sym == TOK_SHIFT_RIGHT;
+            const char *end = tok_scan + (is_double ? 2 : 1);
+            add_token(sym, end);
+            tok_scan = (char *)end - 1;
         }
     }
 
